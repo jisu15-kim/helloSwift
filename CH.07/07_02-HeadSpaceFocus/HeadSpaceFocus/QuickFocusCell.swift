17acad52effd58e728120b69b51c5c467687c717
김지수 <jisu_kim@gimjisuui-MacBookAir.local>
2022-08-02 02:35:16 +0900

    Navigation_HeadTitle

---
+++ CH.07/07_02-HeadSpaceFocus/HeadSpaceFocus/QuickFocusCell.swift
@@ -0,0 +1,23 @@
+//
+//  QuickFocusCell.swift
+//  HeadSpaceFocus
+//
+//  Created by 김지수 on 2022/08/02.
+//
+
+import UIKit
+
+class QuickFocusCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configure(_ quickFocus: QuickFocus) {
+        thumbnailImageView.image = UIImage(named: quickFocus.imageName)
+        titleLabel.text = quickFocus.title
+        descriptionLabel.text = quickFocus.description
+    }
+    
+    
+}
