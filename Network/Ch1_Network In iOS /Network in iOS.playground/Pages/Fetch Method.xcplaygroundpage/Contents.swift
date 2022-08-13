d2ab62eb2e3e04f780892b971bde5080e6ecafa6
jisu-kim <jisu15.kim@gmail.com>
2022-08-13 22:29:49 +0900

    Github Profile APP with Network

--- Network/Ch1_Network In iOS /Network in iOS.playground/Pages/Fetch Method.xcplaygroundpage/Contents.swift
+++ Network/Ch1_Network In iOS /Network in iOS.playground/Pages/Fetch Method.xcplaygroundpage/Contents.swift
@@ -3,11 +3,11 @@
 import Foundation
 
 enum NetworkError: Error {
-  case invalidRequest
-  case transportError(Error)
-  case responseError(statusCode: Int)
-  case noData
-  case decodingError(Error)
+    case invalidRequest
+    case transportError(Error)
+    case responseError(statusCode: Int)
+    case noData
+    case decodingError(Error)
 }
 
 struct GithubProfile: Codable {
@@ -26,10 +26,63 @@ struct GithubProfile: Codable {
     }
 }
 
+final class NetworkService {
+    let session: URLSession
+    init(configuration: URLSessionConfiguration) {
+        session = URLSession(configuration: configuration)
+    }
+    
+    func fetchProfile(userName: String, completion: @escaping (Result<GithubProfile, Error>) -> Void) {
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration)
+        let url = URL(string: "https://api.github.com/users/\(userName)")!
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                completion(.failure(NetworkError.transportError(error)))
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse,
+               !(200..<300).contains(httpResponse.statusCode)
+            {
+                completion(.failure(NetworkError.responseError(statusCode: httpResponse.statusCode)))
+                return
+            }
+            
+            guard let data = data else { return }
+            // data -> GithubProfile
+            
+            do {
+                let decoder = JSONDecoder()
+                let profile = try decoder.decode(GithubProfile.self, from: data)
+                return completion(.success(profile))
+            } catch let error as NSError {
+                return completion(.failure(NetworkError.decodingError(error)))
+            }
+            
+        }
+        task.resume()
+        
+        //    let result = String(data: data, encoding: .utf8)
+        //    print(result)
+    }
+}
 
 
+// Network 담당 networkService를 만듬
+// NetworkService 이용한 네트워크 작업
 
+let networkService = NetworkService(configuration: .default)
 
-
+networkService.fetchProfile(userName: "cafielo") { result in
+    switch result {
+    case .success(let profile):
+        print("Profile: \(profile)")
+    case.failure(let error):
+        print("Error: \(error)")
+    }
+}
 
 //: [Next](@next)
