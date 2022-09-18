59ffe202b431f7c0681b0a79db23528711e75af4
jisu-kim <jisu15.kim@gmail.com>
2022-09-18 12:17:44 +0900

    rps game

---
+++ iOS_Framework/RPSGame/RPSGame/Rps.swift
@@ -0,0 +1,17 @@
+//
+//  Rps.swift
+//  RPSGame
+//
+//  Created by 김지수 on 2022/09/17.
+//
+
+import Foundation
+
+// 원시값(raw value)
+enum Rps: Int {
+    case rock
+    case paper
+    case scissors
+}
+
+// 열거형 같은 경우는 따로 파일을 만들어서 관리
